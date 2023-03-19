91b0159d260140a68e1fa72bb65b45223b572e7d
robinn007 <124763217+robinn007@users.noreply.github.com>
2023-03-19 05:02:30 +0000

    create a math module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const math = require("./math.js");
+console.log(math);
+
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-let result = num1 + num2;
+let result = math.subtract(num1 , num2);
 console.log(`sum of ${num1} and ${num2} is ${result}`)
